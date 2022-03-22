7cb0045219899b7a7013be89b3202086615b0277
Laenar <florian.meu@gmail.com>
2022-03-22 12:37:49 +0100

    Intégration models

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const Sauces = require('./models/sauces.js');
+
+const userName = 'primaryUser@admin';
+const databasePass = 'uPOzov04B9F8jEjh';
+
+mongoose.connect('mongodb+srv://primaryUser:'+databasePass+'@cluster0.rmwua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -11,64 +22,26 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/sauces', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+    delete req.body._id;
+    const sauce = new sauces({
+        ...req.body
+    });
+    Sauces.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
   });
 
 app.get('/api/sauces', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-      {
-        userId :'',
-        name :'fdbvds',
-        manufacturer :'dsvdsv',
-        description : 'sdwvdvdswvdsvdsvdsvdsvdsv',
-        mainPepper : 'dvdsv',
-        imageUrl : 'sdvdsv.csqc',
-        heat :9,
-        likes :10,
-        dislikes : 12,
-        usersLiked : {
+    Sauces.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+});
 
-        },
-        usersDisliked : {}
-      },
-    ];
-    res.status(200).json(stuff);
-  });
-
+app.get('/api/sauces/:id', (req, res, next) => {
+    Sauces.findOne({ _id: req.params.id })
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(404).json({ error }));
+});
 
 
 module.exports = app;
