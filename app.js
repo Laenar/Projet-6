a787d1a0b7d6b93f678e9c0611c322b06bcea4ea
Laenar <florian.meu@gmail.com>
2022-03-23 08:08:01 +0100

    add user.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 
 const saucesRoutes = require('./routes/sauces.js');
+const userRoutes = require('./routes/user.js');
+
 const databasePass = 'uPOzov04B9F8jEjh';
 
 mongoose.connect('mongodb+srv://primaryUser:'+databasePass+'@cluster0.rmwua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -19,6 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
 
--- controllers/auth.js
+++
@@ -1,9 +0,0 @@
-const Sauce = require('../models/auth');
-
-exports.signup = (req, res, next) => {
-   
-};
-  
-exports.login = (req, res, next) => {
-
-};
