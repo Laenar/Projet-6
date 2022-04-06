af0a000f11427d65bbad644cfba0c6c2af88d8b1
Laenar <florian.meu@gmail.com>
2022-04-06 09:01:56 +0200

    add body-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express'); //
+const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require("body-parser");
 
 const saucesRoutes = require('./routes/sauces.js');
 const userRoutes = require('./routes/user.js');
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(bodyParser.json());
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
