4c1a44c427afd7028e5caa24aee9b94cf3973406
Laenar <florian.meu@gmail.com>
2022-04-06 08:07:00 +0200

    test

---
+++ controllers/sauces.js
@@ -0,0 +1,22 @@
+const Sauce = require('../models/sauces');
+
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+};
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(404).json({ error }));
+};
+exports.putOneSauce = (req, res, next) => {
+    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+exports.deleteOneSauce = (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+};
